b2110a0c6333a33fd768a4d9b487dc8d58e68999
Anhelina Hordiienko <anhelina.hordiienko@gmail.com>
2019-07-07 22:24:52 +0300

    add task1

--- FL11_HW6/homework/js/task1.js
+++ FL11_HW6/homework/js/task1.js
@@ -1,1 +1,25 @@
-// Your code goes here
+let a1 = prompt('Please enter any number to asign an abscissa of A. Use point instead of comma if entering decimal.');
+let a2 = prompt('Please enter any number to asign an ordinate of A. Use point instead of comma if entering decimal.');
+let b1 = prompt('Please enter any number to asign an abscissa of B. Use point instead of comma if entering decimal.');
+let b2 = prompt('Please enter any number to asign an ordinate of B. Use point instead of comma if entering decimal.');
+let c1 = prompt('Please enter any number to asign an abscissa of C. Use point instead of comma if entering decimal.');
+let c2 = prompt('Please enter any number to asign an ordinate of C. Use point instead of comma if entering decimal.');
+const devider = 2;
+
+if (a1 && a2 && b1 && b2 && c1 && c2) {
+  a1 = Number(a1);
+  a2 = Number(a2);
+  b1 = Number(b1);
+  b2 = Number(b2);
+  c1 = Number(c1);
+  c2 = Number(c2);
+  if (c1 === (b1 - a1) / devider + a1 && c2 === (b2 - a2) / devider + a2) {
+    console.log(true);
+  } else {
+    console.log(false);
+  }
+} else if (a1 === '' || a2 === '' || b1 === '' || b2 === '' || c1 === '' || c2 === '') {
+  console.log('One or more prompt fields were empty. You may try again. Remember that only numbers are allowed.');
+} else if (a1 === null || a2 === null || b1 === null || b2 === null || c1 === null || c2 === null) {
+  console.log('One or more prompt fields were canceled. You may try again. Remember that only numbers are allowed.');
+}
--- FL11_HW6/homework/js/task2.js
+++ FL11_HW6/homework/js/task2.js
@@ -1,11 +1,12 @@
-let a = Number(prompt('Please enter the positive number for a triangle first side (a). Use point (.) instead of comma (,) if entering decimal fractions'));
-let b = Number(prompt('Please enter the positive number for the triangle second side (b). Use point (.) instead of comma (,) if entering decimal fractions'));
-let c = Number(prompt('Please enter the positive number for the triangle third side (c). Use point (.) instead of comma (,) if entering decimal fractions'));
+let a = Number(prompt('Please enter the positive number for triangle first side (a). Use point if entering decimal'));
+let b = Number(prompt('Please enter the positive number for triangle second side (b). Use point if entering decimal'));
+let c = Number(prompt('Please enter the positive number for triangle third side (c). Use point if entering decimal'));
+
 if (!a || !b || !c || a < 0 || b < 0 || c < 0 || a > b + c || b > a + c || c > a + b) {
   console.log('Triangle doesn’t exist');
-} else if ( a === b && b === c) {
+} else if (a === b && b === c) {
   console.log('Eequivalent triangle');
-} else if ( a === b || a === c || b === c) {
+} else if (a === b || a === c || b === c) {
   console.log('Isosceles triangle');
 } else {
   console.log('Normal triangle');
