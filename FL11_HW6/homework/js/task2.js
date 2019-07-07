bc3d0d75acde6ce64ae3a2ee9a0a8d1c0baf5b55
Anhelina Hordiienko <anhelina.hordiienko@gmail.com>
2019-07-07 16:31:59 +0300

    add sentence in prompt

--- FL11_HW6/homework/js/task2.js
+++ FL11_HW6/homework/js/task2.js
@@ -1,6 +1,6 @@
-let a = Number(prompt('Please enter the positive number for a triangle first side (a).'));
-let b = Number(prompt('Please enter the positive number for the triangle second side (b).'));
-let c = Number(prompt('Please enter the positive number for the triangle third side (c).'));
+let a = Number(prompt('Please enter the positive number for a triangle first side (a). Use point (.) instead of comma (,) if entering decimal fractions'));
+let b = Number(prompt('Please enter the positive number for the triangle second side (b). Use point (.) instead of comma (,) if entering decimal fractions'));
+let c = Number(prompt('Please enter the positive number for the triangle third side (c). Use point (.) instead of comma (,) if entering decimal fractions'));
 if (!a || !b || !c || a < 0 || b < 0 || c < 0 || a > b + c || b > a + c || c > a + b) {
   console.log('Triangle doesn’t exist');
 } else if ( a === b && b === c) {
