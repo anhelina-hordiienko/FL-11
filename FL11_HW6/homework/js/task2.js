44d8bd87f3bb552d8e6d77ec352c17983c7211a4
Anhelina Hordiienko <anhelina.hordiienko@gmail.com>
2019-07-07 14:37:55 +0300

    add task2

--- FL11_HW6/homework/js/task2.js
+++ FL11_HW6/homework/js/task2.js
@@ -1,1 +1,12 @@
-// Your code goes here
+let a = Number(prompt('Please enter the positive number for a triangle first side (a).'));
+let b = Number(prompt('Please enter the positive number for the triangle second side (b).'));
+let c = Number(prompt('Please enter the positive number for the triangle third side (c).'));
+if (!a || !b || !c || a < 0 || b < 0 || c < 0 || a > b + c || b > a + c || c > a + b) {
+  console.log('Triangle doesn’t exist');
+} else if ( a === b && b === c) {
+  console.log('Eequivalent triangle');
+} else if ( a === b || a === c || b === c) {
+  console.log('Isosceles triangle');
+} else {
+  console.log('Normal triangle');
+}
