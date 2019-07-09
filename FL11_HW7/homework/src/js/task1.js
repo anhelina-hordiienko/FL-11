6e7f3c8a3c1514c982bdbca6f88e9c519b15c9cd
Anhelina Hordiienko <anhelina.hordiienko@gmail.com>
2019-07-09 17:57:22 +0300

    add task1

--- FL11_HW7/homework/src/js/task1.js
+++ FL11_HW7/homework/src/js/task1.js
@@ -1,1 +1,45 @@
-// Your code goes here
+const email = prompt('Please enter your email.');
+let password;
+const minEmailLength = 6;
+
+if (!email) {
+  alert('Canceled.');
+} else if (email.length < minEmailLength) {
+  alert('I don\'t know any emails having name length less than 6 symbols');
+} else if (email === 'user@gmail.com' || email === 'admin@gmail.com') {
+  password = prompt('Please enter your password.');
+  if (!password) {
+    alert('Canceled.');
+  } else if (email === 'user@gmail.com' && password !== 'UserPass') {
+    alert('Wrong password.');
+  } else if (email === 'admin@gmail.com' && password !== 'AdminPass') {
+    alert('Wrong password.');
+  }
+
+  let isPassReset = confirm('Do you want to change your password?');
+  if (!isPassReset) {
+    alert('You have failed the change.')
+  } else {
+    const oldPass = prompt('Please enter your current password.');
+    if (!oldPass) {
+      alert('Canceled.');
+    } else if ((email === 'user@gmail.com' && password !== 'UserPass') || (email === 'admin@gmail.com' && password !== 'AdminPass')) {
+      alert('Wrong password.');
+    } else {
+      const newPass = prompt('Please enter a new password.');
+      const minPassLength = 5;
+      if (newPass.length < minPassLength) {
+        alert('It\'s too short password. Sorry.');
+      } else {
+        const newPassRepeat = prompt('Please enter your new password again.');
+        if (newPassRepeat !== newPass) {
+          alert('You wrote the wrong password.');
+        } else {
+          alert('You have successfully changed your password.');
+        }
+      }
+    }
+  }
+} else {
+  alert('I don\'t know you.');
+}
