e6e755b2c56a82b10e4d095cd0bfaa095868eafa
Anhelina Hordiienko <anhelina.hordiienko@gmail.com>
2019-07-12 21:14:09 +0300

    make calculating of hours more simple

--- FL11_HW8/homework/js/formatTime.js
+++ FL11_HW8/homework/js/formatTime.js
@@ -1,6 +1,6 @@
 function formatTime(inputMinutes) {
   const minutes = inputMinutes % 60;
-  const hours = inputMinutes < 1440 ? Math.floor(inputMinutes / 60) : Math.floor((inputMinutes % 1440) / 60); // 1440 is amount of minutes in a day (24*60)
+  const hours = Math.floor((inputMinutes % 1440) / 60); // 1440 is amount of minutes in a day (24*60)
   const days = Math.floor(inputMinutes / 1440);
   return `${days} day(s) ${hours} hour(s) ${minutes} minute(s).`;
 }
