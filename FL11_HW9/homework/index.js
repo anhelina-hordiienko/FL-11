4e5c08ee9da3220549a5943b432fed0569ebd5df
Anhelina Hordiienko <anhelina.hordiienko@gmail.com>
2019-07-16 21:05:42 +0300

    add tasks 5-10

--- FL11_HW9/homework/index.js
+++ FL11_HW9/homework/index.js
@@ -1,6 +1,6 @@
 // task 0
 function getNumbers(inputString) {
-  let returns = [];
+  const returns = [];
   for (let letter of inputString) {
     const returnsInput = parseInt(letter, 10);
     if (returnsInput) {
@@ -33,7 +33,7 @@ function executeforEach(array, callback) {
 
 //task3
 function mapArray(array, callback) {
-  let transformedArray = [];
+  const transformedArray = [];
   executeforEach(array, function (item) {
     transformedArray.push(callback(item));
   });
@@ -42,11 +42,59 @@ function mapArray(array, callback) {
 
 //task4
 function filterArray(array, callback) {
-  let filteredArray = [];
+  const filteredArray = [];
   executeforEach(array, function (item) {
     if (callback(item)) {
       filteredArray.push(item);
     }
   });
   return filteredArray;
+}
+
+//task5
+function showFormattedDate(date) {
+  return `Date: ${date.toLocaleString('en-us', { month: 'short' })} ${date.getDate()} ${date.getFullYear()}`;
+}
+
+//task6 
+function canConvertToDate(date) {
+  return new Date(date) !== 'Invalid Date' && !isNaN(new Date(date));
+}
+
+//task7
+function daysBetween(date1, date2) {
+  const millisInDay = 86400000;
+  const diffTime = Math.abs(date2.getTime() - date1.getTime());
+  return Math.ceil(diffTime / millisInDay);
+}
+
+//task8
+function getAmountOfAdultPeople(data) {
+  const daysOfEighteenYears = 6570;
+  const adults = filterArray(data, (person) => {
+    daysBetween(new Date(person[' birthday ']), new Date()) > daysOfEighteenYears
+  });
+  return adults.length;
+}
+
+//task9
+function keys(object) {
+  const keysArray = [];
+  for(let key in object) {
+    if (object.hasOwnProperty(key)) {
+      keysArray.push(key);
+    }
+  }
+  return keysArray;
+}
+
+//task10
+function values(object) {
+  const valuesArray = [];
+  for(let key in object) {
+    if (object.hasOwnProperty(key)) {
+      valuesArray.push(object[key]);
+    }
+  }
+  return valuesArray;
 }
