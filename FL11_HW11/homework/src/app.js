a226e422a4c9bd1f6cfec9ceb9d70278771ae645
Anhelina Hordiienko <anhelina.hordiienko@gmail.com>
2019-07-21 22:20:24 +0300

    add html, css and js(create an item)

--- FL11_HW11/homework/src/app.js
+++ FL11_HW11/homework/src/app.js
@@ -1,3 +1,44 @@
-let rootNode = document.getElementById("root");
+let rootNode = document.getElementById('root');
 
 // Your code goes here
+function notEmpty(e) {
+  const button = e.target.nextElementSibling;
+  if(e.target.value) {
+    button.classList.remove('md-inactive');
+  } else {
+    button.classList.add('md-inactive');
+  }
+}
+
+document.getElementById('add').addEventListener('click', function() {
+  let value = document.getElementById('item').value;
+  if (value) {
+    addItemToDo(value);
+    document.getElementById('item').value = '';
+  } else {
+    alert('Plese, enter some value in the text field.');
+  }
+});
+
+function addItemToDo(text) {
+  const li = document.createElement('li');
+
+  const label = document.createElement('label');
+  label.innerHTML = '<input type="checkbox" name="affairs"></input>';
+  const span = document.createElement('span');
+  span.innerText = text;
+  label.appendChild(span);
+
+
+  const edit = document.createElement('i');
+  edit.classList.add('material-icons');
+  edit.innerText = 'edit';
+
+  const trash = document.createElement('i');
+  trash.className = 'material-icons trash';
+  trash.innerText = 'delete';
+  li.appendChild(label);
+  li.appendChild(edit);
+  li.appendChild(trash);
+  document.querySelector('ul').appendChild(li);
+}
